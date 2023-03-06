7cdc1dc45d4585c82aa55f5269c0fe8ea3f59648
oliver7100 <59563775+oliver7100@users.noreply.github.com>
2023-03-06 22:03:31 +0100

    Result

---
+++ House4It/House4It/PrisListe.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LINQtoCSV;
+
+
+namespace House4It
+{
+
+    [Serializable]
+    public class PrisListe
+    {
+
+        [CsvColumn(Name = "Item", FieldIndex = 1)]
+        public string Item { get; set; }
+
+        [CsvColumn(Name = "article description", FieldIndex = 2)]
+        public string ArticleDescription { get; set; }
+
+        [CsvColumn(Name = "unit", FieldIndex = 3)]
+        public string Unit { get; set; }
+
+        [CsvColumn(Name = "kostpris EUR", FieldIndex = 4)]
+        public string KostPrisEur { get; set; }
+
+        [CsvColumn(Name = "price unit", FieldIndex = 5)]
+        public string PriceUnit { get; set; }
+
+        [CsvColumn(Name = "price group", FieldIndex = 6)]
+        public string PriceGroup { get; set; }
+
+        [CsvColumn(Name = "date of issuance", FieldIndex = 7, OutputFormat = "dd-MM-yyyy HH:mm")]
+        public string DateOfIssuance { get; set; }
+
+    }
+
+    public class ResultProduct
+    {
+        [CsvColumn(Name = "Item", FieldIndex = 1)]
+        public string varenummer { get; set; }
+
+        [CsvColumn(Name = "article description", FieldIndex = 2)]
+        public string navn { get; set; }
+
+        [CsvColumn(Name = "kostpris i danske kroner", FieldIndex = 3)]
+        public string price { get; set; }
+
+        [CsvColumn(Name = "beregnet salgspris i danske kroner.", FieldIndex = 4)]
+        public string salesPrice { get; set; }
+    }
+}
---
+++ House4It/House4It/Program.cs
@@ -0,0 +1,107 @@
+﻿using House4It;
+using LINQtoCSV;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+       ReadCsvFile();
+
+        Console.ReadKey();
+
+    }
+
+
+
+    private static void ReadCsvFile()
+    {
+        var csvFileDescription = new CsvFileDescription
+        {
+            FirstLineHasColumnNames = true,
+            IgnoreUnknownColumns = true,
+            SeparatorChar = ';',
+            UseFieldIndexForReadingData = false
+        };
+
+        var csvContext = new CsvContext();
+        var priceLists = csvContext.Read<PrisListe>("Prisliste.csv", csvFileDescription);
+
+
+        var newResult = new List<ResultProduct>();
+
+        foreach (var priceListe in priceLists)
+        {
+            var result = new ResultProduct();
+
+            //Vi Cutter stringen, da den indeholder euro tegn.
+            string Cut = priceListe.KostPrisEur.Substring(0, priceListe.KostPrisEur.Length - 2);
+            //Vi replacer her da C# læser komma anderledes. 
+            Cut = Cut.Replace(',', '.');
+            
+
+            //Parser string til double og regner det om til DK
+            double Euro = double.Parse(Cut);
+            double dk = Euro * 7.44;
+
+            double Round = Math.Round(dk);
+
+            result.price = Round.ToString() + " DK";
+
+            //Replacer alle med både stort B og lille b til sort
+            var Name = priceListe.ArticleDescription.Replace("Black", "sort");
+            var nName = Name.Replace("black", "sort");
+
+            result.navn = nName;
+
+            result.varenummer = priceListe.Item;
+
+
+            
+            //Fortjensten udregning
+            double Percentage = dk / 100;
+
+            if (priceListe.PriceGroup == "21") 
+            {
+                Percentage = Percentage * 30;
+                Percentage = Math.Round(Percentage);
+            }
+            else if (priceListe.PriceGroup == "23" || priceListe.PriceGroup == "24") 
+            {
+                Percentage = Percentage * 40;
+                Percentage = Math.Round(Percentage);
+
+            }
+            else if (priceListe.PriceGroup == "22" || priceListe.PriceGroup == "25") 
+            {
+                Percentage = Percentage * 50;
+                Percentage = Math.Round(Percentage);
+
+            }
+
+            result.salesPrice = Percentage.ToString() + " DK";
+
+
+            //Tilføjer de ny koloner til en ny oprettet CSV fil.
+            newResult.Add(result);
+            
+         
+
+            Console.WriteLine($"{priceListe.Item} | {priceListe.ArticleDescription} |  {priceListe.KostPrisEur} ");
+            Console.WriteLine(" ");
+        }
+
+
+
+        CsvFileDescription outputFileDescription = new CsvFileDescription
+        {
+            SeparatorChar = ';',
+            FileCultureName = "da-DK"
+        };
+
+        CsvContext cc = new CsvContext();
+        cc.Write(
+            newResult.ToArray(),
+            "./result.csv",
+            outputFileDescription);
+    }
+}
